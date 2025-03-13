7c033358fb24d95bd00488efcc09b58d834407a3
deymosko <maximstoyka1@gmail.com>
2025-03-13 14:14:50 +0100

    Install commit

---
+++ src/test/java/com/detmosko/task/testbot/TestbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detmosko.task.testbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
